bf2ce094d4ba67be330469546b82f17138599149
Sam Wilson <sawilson@akamai.com>
2018-12-09 19:52:32 -0500

    Rough implementation of header auth module (#5)

---
+++ bellhop-auth-header/src/lib.rs
@@ -0,0 +1,88 @@
+use bellhop::auth::*;
+use bellhop::models::user::User;
+
+use diesel::prelude::*;
+
+use regex::Regex;
+
+use rocket::{Outcome, Rocket};
+use rocket::fairing::AdHoc;
+use rocket::request::{Request, State};
+
+#[derive(Debug)]
+struct AuthRegex {
+    header_name: String,
+    re: Regex,
+}
+
+#[derive(Debug)]
+pub struct Header;
+
+const DEFAULT: &str = "(?P<email>.*)";
+
+impl<B, Conn> Auth<B, Conn> for Header
+where
+    Conn: Connection<Backend = B>,
+    B: diesel::backend::Backend<RawValue = [u8]>,
+{
+    fn prelaunch(&self, rocket: Rocket) -> Rocket {
+        rocket.attach(AdHoc::on_attach("Auth Header Config", |rocket| {
+            let name = rocket
+                .config()
+                .get_str("auth_header")
+                .unwrap_or("X-Bellhop-Email");
+
+            let re_str = rocket
+                .config()
+                .get_str("auth_header_email_pattern")
+                .unwrap_or(DEFAULT);
+
+            let re = match Regex::new(re_str) {
+                Ok(x) => x,
+                Err(_) => return Err(rocket),
+            };
+
+            let state = AuthRegex {
+                re,
+                header_name: name.to_owned(),
+            };
+
+            Ok(rocket.manage(state))
+        }))
+    }
+
+    fn authenticate(&self, c: &Conn, req: &Request) -> Result<Option<User>, Error> {
+        let auths = match req.guard::<State<AuthRegex>>() {
+            Outcome::Success(x) => x,
+            Outcome::Failure(_) => return Err(Error::with_msg("unable to get AuthRegex")),
+            Outcome::Forward(_) => return Ok(None),
+        };
+
+        let header = match req.headers().get_one(&auths.header_name) {
+            None | Some("") => return Ok(None),
+            Some(x) => x,
+        };
+
+        let mut email = None;
+
+        for capture in auths.re.captures_iter(header) {
+            if let Some(x) = capture.name("email") {
+                email = Some(x.as_str());
+                break;
+            }
+        }
+
+        let email = match email {
+            Some(x) => x,
+            None => return Ok(None),
+        };
+
+        let user = User::by_email(c, email)
+            .map_err(Error::for_kind(ErrorKind::msg("unable to get user for authentication")))?;
+
+        match user {
+            None => unimplemented!(),
+            x => Ok(x),
+        }
+    }
+}
--- bellhop-bin/src/main.rs
+++ bellhop-bin/src/main.rs
@@ -1,11 +1,24 @@
+//! bellhop-bin is an example of how to attach different authentication and hook
+//! modules to the bellhop library.
+//!
+//! While you can run this without customization, it probably won't be what you
+//! want.
+
 use bellhop::Bellhop;
 
 use bellhop_auth_dummy::Dummy;
+
+use bellhop_auth_header::Header;
 
 use bellhop_hook_email::Email;
 
 use bellhop_hook_jenkins::Jenkins;
 
 fn main() {
-    Bellhop::default().auth(Dummy).hook(Email).hook(Jenkins).start()
+    Bellhop::default()
+        .auth(Header)       // Allow logging in based on the value of 'X-Bellhop-Email' header.
+        .auth(Dummy)        // Allow logging in with just an email address.
+        .hook(Email)        // Sends emails when a lease is close to expiring.
+        .hook(Jenkins)      // Triggers a jenkins build on certain hook events.
+        .start()            // Start running the server.
 }
