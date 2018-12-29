1d961251f45090cf5e08bfa9e2d76ded505a4a15
Sam Wilson <sawilson@akamai.com>
2018-12-29 18:20:34 -0500

    Make the email hook configurable

--- bellhop-bin/src/main.rs
+++ bellhop-bin/src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     Bellhop::default()
         .auth(Header) // Allow logging in based on the value of 'X-Bellhop-Email' header.
         .auth(Dummy) // Allow logging in with just an email address.
-        .hook(Email) // Sends emails when a lease is close to expiring.
+        .hook(Email::new()) // Sends emails when a lease is close to expiring.
         .hook(Jenkins) // Triggers a jenkins build on certain hook events.
         .start() // Start running the server.
 }
--- bellhop-hook-email/src/lib.rs
+++ bellhop-hook-email/src/lib.rs
@@ -21,28 +21,139 @@
 //!
 //! fn main() {
 //!     Bellhop::default()
-//!         .hook(Email)
+//!         .hook(Email::new())
 //!         .start()
 //! }
 //! ```
 
 #![deny(missing_docs)]
 
+#[macro_use]
+extern crate serde_derive;
+
 use bellhop::db::Db;
 use bellhop::hooks::{Data, Error, ErrorKind, Hook};
 use bellhop::models::user::User;
 
+use lettre::smtp::authentication::{Credentials, Mechanism};
+use lettre::smtp::client::net::{ClientTlsParameters, DEFAULT_TLS_PROTOCOLS};
 use lettre::{ClientSecurity, SmtpClient, Transport};
 
 use lettre_email::EmailBuilder;
 
+use native_tls::TlsConnector;
+
+use rocket::fairing::AdHoc;
+use rocket::Rocket;
+
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+enum ClientSecurityConfig {
+    None,
+    Opportunistic,
+    Required,
+    Wrapper,
+}
+
+impl Default for ClientSecurityConfig {
+    fn default() -> Self {
+        ClientSecurityConfig::Required
+    }
+}
+
+fn default_subject() -> String {
+    "Bellhop Reservation Expiry Warning".to_owned()
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Config {
+    from: String,
+
+    #[serde(default = "default_subject")]
+    subject: String,
+
+    smtp_host: String,
+    smtp_port: u16,
+
+    #[serde(default)]
+    smtp_client_security: ClientSecurityConfig,
+
+    #[serde(default)]
+    smtp_credentials: Option<Credentials>,
+
+    #[serde(default)]
+    smtp_auth_mechanism: Option<Mechanism>,
+}
+
+impl Config {
+    fn tls_parameters(&self) -> ClientTlsParameters {
+        let mut tls_builder = TlsConnector::builder();
+        tls_builder.min_protocol_version(Some(DEFAULT_TLS_PROTOCOLS[0]));
+
+        let connector = tls_builder.build().unwrap();
+
+        ClientTlsParameters::new(self.smtp_host.clone(), connector)
+    }
+
+    pub fn create_client(&self) -> SmtpClient {
+        let client_security = match self.smtp_client_security {
+            ClientSecurityConfig::None => ClientSecurity::None,
+            ClientSecurityConfig::Required => ClientSecurity::Required(self.tls_parameters()),
+            ClientSecurityConfig::Wrapper => ClientSecurity::Wrapper(self.tls_parameters()),
+            ClientSecurityConfig::Opportunistic => {
+                ClientSecurity::Opportunistic(self.tls_parameters())
+            }
+        };
+
+        let mut client =
+            SmtpClient::new((self.smtp_host.as_str(), self.smtp_port), client_security).unwrap();
+
+        if let Some(ref creds) = self.smtp_credentials {
+            client = client.credentials(creds.clone());
+        }
+
+        if let Some(ref mech) = self.smtp_auth_mechanism {
+            client = client.authentication_mechanism(mech.clone());
+        }
+
+        client
+    }
+}
+
 /// Sends email when leases are about to expire.
 ///
 /// See the crate documentation for more information.
-#[derive(Debug)]
-pub struct Email;
+#[derive(Debug, Default)]
+pub struct Email {
+    config: Arc<Mutex<Option<Config>>>,
+}
+
+impl Email {
+    /// Create a new instance of `Email`.
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
 
 impl Hook for Email {
+    fn prelaunch(&self, rocket: Rocket) -> Rocket {
+        let config_slot = self.config.clone();
+
+        rocket.attach(AdHoc::on_attach("Hook Email Config", move |rocket| {
+            let config: Config = rocket
+                .config()
+                .get_extra("hook_email")
+                .unwrap()
+                .clone()
+                .try_into()
+                .unwrap();
+
+            *config_slot.lock().unwrap() = Some(config);
+            Ok(rocket)
+        }))
+    }
+
     fn warned(&self, db: &Db, data: Data) -> Result<(), Error> {
         let lease = data.lease();
 
@@ -55,23 +166,23 @@ impl Hook for Email {
                 return Err(Error::with_msg(format!(
                     "No userid found for lease: {:?}",
                     lease
-                )))
+                )));
             }
         };
+
+        let config = self.config.lock().unwrap().as_ref().unwrap().clone();
 
         let email = EmailBuilder::new()
             .to((user.email(), "Bellhop User"))
             // ... or by an address only
-            .from("sheriff.bellhop@example.com")
-            .subject("Bellhop Reservation Expiry Warning")
+            .from(config.from.as_str())
+            .subject(config.subject.as_str())
             .text(format!("This is the bellhop Sheriff letting you know that your reservation (id: {}) is going to expire soon! Best of luck.",user.id()))
             .build()
             .unwrap();
 
-        // Open a local connection on port 25
-        let mut mailer = SmtpClient::new("smtp.example.com:25", ClientSecurity::None)
-            .unwrap()
-            .transport();
+        let mut mailer = config.create_client().transport();
+
         // Send the email
         let result = mailer.send(email.into());
 
