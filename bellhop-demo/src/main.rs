1fbd12570e0680a2a1f6e40a2b3914f262e219e6
Sam Wilson <sawilson@akamai.com>
2019-01-08 15:16:44 -0500

    Remove bellhop-hook-email from bellhop-demo

--- bellhop-demo/src/main.rs
+++ bellhop-demo/src/main.rs
@@ -12,15 +12,13 @@ use bellhop_auth_dummy::Dummy;
 
 use bellhop_auth_header::Header;
 
-use bellhop_hook_email::Email;
-
 use bellhop_hook_jenkins::Jenkins;
 
 fn main() {
     Bellhop::default()
         .auth(Header) // Allow logging in based on the value of 'X-Bellhop-Email' header.
         .auth(Dummy) // Allow logging in with just an email address.
-        .hook(Email::new()) // Sends emails when a lease is close to expiring.
+        // .hook(Email::new()) // Sends emails when a lease is close to expiring.
         .hook(Jenkins) // Triggers a jenkins build on certain hook events.
         .start() // Start running the server.
 }
