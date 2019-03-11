1695cee217577210ebe31df12d02599bad8f4ce8
Sam Wilson <sawilson@akamai.com>
2019-03-11 19:48:33 +0000

    Add show/delete/create for AssetTypes in cli

--- bellhop-cli/src/cmd/asset_types.rs
+++ bellhop-cli/src/cmd/asset_types.rs
@@ -1,11 +1,12 @@
 //! Command for interacting with Bellhop's asset types.
 use bellhop_client::apis::client::APIClient;
+use bellhop_client::models::CreateAssetType;
 
 use crate::config::Config;
 
-use super::{Api, CmdError};
-
 use structopt::StructOpt;
+
+use super::{Api, CmdError};
 
 #[derive(Debug, StructOpt)]
 pub struct AssetTypes {
@@ -22,6 +23,9 @@ impl AssetTypes {
 
         match self.cmd {
             Cmd::List => self.list(client),
+            Cmd::Show(ref show) => self.show(client, show),
+            Cmd::Delete(ref show) => self.delete(client, show),
+            Cmd::Create(ref create) => self.create(client, create),
         }
     }
 
@@ -35,10 +39,75 @@ impl AssetTypes {
 
         Ok(())
     }
+
+    fn delete(&self, client: APIClient, show: &ById) -> Result<(), CmdError> {
+        let api = client.default_api();
+
+        let asset_type = api.delete_asset_type(show.id)?;
+
+        let at = serde_json::to_string_pretty(&asset_type).unwrap();
+        println!("{}", at);
+
+        Ok(())
+    }
+
+    fn show(&self, client: APIClient, show: &ById) -> Result<(), CmdError> {
+        let api = client.default_api();
+
+        let asset_type = api.show_asset_type(show.id)?;
+
+        let at = serde_json::to_string_pretty(&asset_type).unwrap();
+        println!("{}", at);
+
+        Ok(())
+    }
+
+    fn create(&self, client: APIClient, show: &Create) -> Result<(), CmdError> {
+        let api = client.default_api();
+
+        let create_asset_type = CreateAssetType::new(show.name.clone(), show.plural_name.clone());
+
+        let asset_type = api.create_asset_type(create_asset_type)?;
+
+        let at = serde_json::to_string_pretty(&asset_type).unwrap();
+        println!("{}", at);
+
+        Ok(())
+    }
 }
 
 #[derive(Debug, StructOpt)]
 enum Cmd {
     #[structopt(name = "list", about = "Prints a list of asset types")]
     List,
+
+    #[structopt(name = "show", about = "Print a single asset type")]
+    Show(ById),
+
+    #[structopt(
+        name = "delete",
+        about = "Deletes an asset type and all of its tags and assets"
+    )]
+    Delete(ById),
+
+    #[structopt(name = "create", about = "Create a new asset type")]
+    Create(Create),
+}
+
+#[derive(Debug, StructOpt)]
+struct ById {
+    #[structopt(help = "The unique identifier for the asset type")]
+    id: i32,
+}
+
+#[derive(Debug, StructOpt)]
+struct Create {
+    #[structopt(long = "name", help = "Name of the asset type to be created")]
+    name: String,
+
+    #[structopt(
+        long = "plural-name",
+        help = "Spelling of the name, when referring to many"
+    )]
+    plural_name: String,
 }
